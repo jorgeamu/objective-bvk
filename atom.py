8a7dd3fc8d7ca087b8a3e59f75bdf8ee7e1e1a10
fredy737 <fredy737@caltech.edu>
2014-10-22 15:29:26 -0700

    Update atom.py

--- atom.py
+++ atom.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 
-print("Hello world")
+# Here's a very rough start to the atom class.
+
+class atom(object):
+    
+    def __init__(self,atom_type,num_id,position):
+        self.atom_type = atom_type
+        self.num_id = num_id
+        self.position = position
+        
+        if self.atom_type == "Fe":
+            self.mass = 55.845 # In amu
+        elif self.atom_type == "Ti":
+            self.mass = 47.867 # In amu
+        
+    def update_position(self):  
