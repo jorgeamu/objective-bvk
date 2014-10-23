507d300a74e949d4b3dc07627116da7e84eb798c
fredy737 <fredy737@caltech.edu>
2014-10-22 19:34:42 -0700

    Update atom.py

--- atom.py
+++ atom.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
-# Here's a very rough start to the atom class. Haven't dealt with nearest neighbors yet.
+# Here's a very rough start to the atom class.
 
 class atom(object):
     
-    def __init__(self,atom_type,num_id,position):
-        self.atom_type = atom_type
-        self.num_id = num_id
-        self.position = position
+    def __init__(self,atom_type,num_id,position,first_nn,second_nn):
+        self.atom_type = atom_type      # Fe or Ti
+        self.num_id = num_id            # Number ID (1-128)
+        self.position = position        # Atom's position
+        self.first_nn = first_nn        # 1st nearest neighbors
+        self.second_nn = second_nn      # 2nd nearest neighbors
         
         if self.atom_type == "Fe":
             self.mass = 55.845 # In amu
